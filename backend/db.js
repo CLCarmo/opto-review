0964620287a78bd0786f614ee4819c186dedb9d2
CLCarmo <caiolucasprofissional@gmail.com>
2025-11-29 21:12:02 -0300

    Corrigindo conexao do banco para producao

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,18 @@
 // backend/db.js
 const { Pool } = require('pg');
 
-// Configuração correta para o Railway (Produção) e Local
+// Configuração para usar a Variável de Ambiente do Railway
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // Necessário para conectar no Railway
+    rejectUnauthorized: false // Obrigatório para funcionar no Railway
   }
+});
+
+// Evento para monitorar erros inesperados no pool
+pool.on('error', (err, client) => {
+  console.error('Erro inesperado no cliente do pool', err);
+  process.exit(-1);
 });
 
 module.exports = {
